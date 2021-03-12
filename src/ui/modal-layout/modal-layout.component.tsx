2c62d54e48eac2070a7fd5a806e19e2c6105544f
Giedrius Blažulionis <giedrius.bla@gmail.com>
2021-03-12 14:31:50 +0200

    lock body scroll on modal open

--- src/ui/modal-layout/modal-layout.component.tsx
+++ src/ui/modal-layout/modal-layout.component.tsx
@@ -2,18 +2,23 @@ import { FC } from 'react';
 import FocusTrap from 'focus-trap-react';
 
 import 'ui/modal-layout/modal-layout.styles.scss';
+import { useBodyScrollLock } from 'utils/use-body-scroll-lock';
 
 interface Props {
   titleId: string;
   onClose: () => void;
 }
 
-export const ModalLayout: FC<Props> = ({ titleId, onClose, children }) => (
-  <FocusTrap>
-    <div className="modal" aria-modal aria-labelledby={titleId} role="dialog">
-      <div className="modal__backdrop" onClick={onClose} aria-hidden />
+export const ModalLayout: FC<Props> = ({ titleId, onClose, children }) => {
+  useBodyScrollLock();
 
-      <div className="modal__content">{children}</div>
-    </div>
-  </FocusTrap>
-);
+  return (
+    <FocusTrap>
+      <div className="modal" aria-modal aria-labelledby={titleId} role="dialog">
+        <div className="modal__backdrop" onClick={onClose} aria-hidden />
+
+        <div className="modal__content">{children}</div>
+      </div>
+    </FocusTrap>
+  );
+};
